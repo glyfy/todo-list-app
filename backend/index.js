eb3cb6f710fb651c3644dea84d5f655f5f1f764f
Glen Lim <glenlim@Glens-MacBook-Pro.local>
2026-01-25 11:42:07 +0800

    Add backnd code and auth API Add README.md Add .gitignore

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+require("dotenv").config()
+const express = require("express")
+const pool = require('./db')
+const tasksRouter = require("./routes/tasks")
+const authRouter = require("./routes/auth")
+const requireAuth = require("./middleware/requireAuth")
+
+const app = express()
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("Hello from Express")
+})
+
+app.get("/health", (req, res) => {
+    res.json({ok: true})
+})
+
+app.get("/health/db", async (req, res) => {
+    try {
+        console.log(process.env.DATABASE_URL)
+        const result = await pool.query("select now() as server_time;");
+        res.json({db: "ok", time: result.rows[0].server_time })
+    } catch(err) {
+        console.error(err)
+        res.status(500).json({ db : "error" })
+    }
+})
+
+app.use("/auth", authRouter)
+// app.use("/tasks", requireAuth, tasksRouter)
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on http://localhost:${process.env.PORT}`)
+})
---
+++ backend/routes/auth.js
@@ -0,0 +1,82 @@
+const express = require("express")
+const router = express.Router()
+const pool = require("../db")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+
+router.post("/register", async (req, res) => {
+    try {
+        // get the name, email and password
+        const { name, email, password } = req.body
+        // if missing any data, throw status 400
+        if (!name || !email || !password) {
+            return res.status(400).json({error: "Need name, email and password"})
+        }
+        // if password too short, throw status 400
+        if (password.length < 8) {
+            return res.status(400).json({error: "Password needs to be longer"})
+        }
+        // encrypt the password with bcrypt
+        const hashed_password = await bcrypt.hash(password, 10)
+
+        // insert into db using string + list of variables
+        const result = await pool.query(
+            `insert into public.users (email, password, name)
+            values ($1, $2, $3)
+            returning id, email, name, created_at;            
+            `, [email.toLowerCase(), hashed_password, name]
+        )
+        // return status 201 with the newly added user
+        return res.status(201).json({user: result.rows[0]})
+    } catch(error) {
+        // return status code and useful info to frontend about error
+        if (error.code == "23505"){
+            return res.status(409).json({error: "Email already exists in DB"})
+        }
+        console.error(error)
+        return res.status(500).json({error:"Internal server error"})
+    }
+})
+
+router.post("/login", async (req, res) => {
+    try {
+        // deconstruct the req.body
+        const { email, password } = req.body
+        // validate login details to make sure not empty
+        if (!email || !password) {
+            return res.status(400).json({error:"Need email and password"})
+        }
+        // query db with a select using email
+        const result = await pool.query(
+            `select id, name, email, password, created_at from public.users
+            where email=$1 
+            `, [email]
+        )
+        // if length of rows is 0
+        if (result.rows.length == 0) {
+            // return error user does not exist
+            return res.status(401).json({error:"Invalid credentials"})
+        }
+        const user = result.rows[0]
+        // compare password with hashed password
+        const ok = await bcrypt.compare(password, user.password)
+        if (!ok) {
+            return res.status(401).json({error:"Invalid credentials"})
+        }
+        // issue json webtoken
+        const token = jwt.sign(
+            {id: user.id, email: user.email},
+            process.env.JWT_SECRET_TOKEN,
+            {expiresIn: "7d"}
+        ) 
+        // return status200 and user json
+        return res.status(200).json({token, user: {...user} })
+    } catch(error) {
+        console.error(error)
+        // internal server error
+        return res.status(500).json({error:"Internal server error"}) 
+    }
+
+})
+
+module.exports = router
