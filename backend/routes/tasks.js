241e0aa205275565de56984737f757dc00ed682e
Glen Lim <glenlim@Glens-MacBook-Pro.local>
2026-01-27 15:32:02 +0800

    Add patch task api endpoint (in progress)

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -19,24 +19,59 @@ router.get("/", async (req, res) =>{
 
 router.post("/", async (req, res) => {
     try {
-        let  { user_id, title, startdate, due_at, completed_at } = req.body
+        let  { title, startdate, due_at, completed_at } = req.body
+        // ensure title is not empty and is string
+        if (!title?.trim?.()){
+            return res.status(401).json({error: "Title cannot be empty"})
+        }
+        title = title.trim()
         // sanitize strings
-        startdate = startdate || null
-        due_at = due_at || null
-        completed_at = completed_at || null
+        startdate = startdate?.trim?.() || null
+        due_at = due_at?.trim?.() || null
+        completed_at = completed_at?.trim?.() || null
+        // ensure due_at > startdate
+        if (startdate &&  due_at) {
+            const startTs = new Date(start_at).getTime()
+            const dueTs = new Date(due_at).getTime()
+
+            if (Number.isNaN(startTs) || Number.isNan) {
+                return res.status(400).json({error:"Invalid date format"})
+            }
+
+            if (dueTs <= startTs) {
+                return res.status(400).json({
+                    error:"due date must be later than start date"
+                })
+            }
+        }
         // make db insert query
         const result = await pool.query(
         `
         insert into public.tasks (user_id, title, startdate, due_at, completed_at)
         values ($1, $2, $3, $4, $5)
         returning *
-        `, [user_id, title, startdate, due_at, completed_at])
+        `, [req.user.id, title, startdate, due_at, completed_at])
         const task = result.rows[0]
-        return res.status(201).json({...task})
+        return res.status(201).json({ task: result.rows[0] })
 
     } catch(error) {
+        console.error("POST /tasks error", error)
         // res status 500
         return res.status(500).json({error:"Internal server error"})
+    }
+})
+
+router.patch("/:id", async (req, res) => {
+    try {
+        // read title, change startdate, dueat, completedat from req.body
+        const {title, startdate, due_at} = req.body
+        // if title is empty or not string throw error
+        
+        // for dates if empty or not string throw error
+        // send update query to db
+        // return status 200 and updated task
+    } catch(error) {
+        // status 500
     }
 })
 
