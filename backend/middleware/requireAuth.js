0dd23c1902bb31dac1fb1c7e1a3a93232825cfcf
Glen Lim <glenlim@Glens-MacBook-Pro.local>
2026-02-05 14:51:12 +0800

    Remove comments from requireauth middleware

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -8,23 +8,6 @@ function requireAuth(req, res, next) {
   }
 
   try {
-    // const authHeader = req.headers.authorization;
-    // // if authorization header not exists, return null
-    // if (authHeader == null) {
-    //   return res.status(401).json({ error: "Missing Authorization Header" });
-    // }
-    // // check for correct jwt format
-    // const parts = authHeader.split(" ");
-    // if (parts.length < 2 || parts[0] != "Bearer") {
-    //   return res.status(401).json({ error: "Invalid Authorization format" });
-    // }
-    // const token = parts[1];
-    // // verify jwt
-    // const decoded = jwt.verify(token, process.env.JWT_SECRET_TOKEN);
-    // // add userid to req
-    // req.user = { id: decoded.id };
-    // // call next
-
     const payload = jwt.verify(token, process.env.JWT_SECRET_TOKEN);
     console.log(payload);
     req.user_id = payload.user_id;
